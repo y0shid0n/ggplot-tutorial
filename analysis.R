fa5700779c58f6f3f2f987900e7768a07f565e5a
NaohiroYoshida <y0shid0n1031@gmail.com>
2021-10-09 14:31:41 +0900

    initial commit

---
+++ analysis.R
@@ -0,0 +1,36 @@
+# ggplotのれんしゅう
+# MFから持ってきたcsvでグラフを書く
+
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(readxl)
+library(ggplot2)
+library(lubridate)
+library(stringr)
+
+# csv読み込み
+files <- list.files(path="./csv", pattern="*.csv", full.names=T)
+
+myreadcsv <- function(file) {
+  tmp <- read.csv(file, fileEncoding="cp932") %>%
+    rename_with(~str_remove_all(., "\\..+\\."))
+  colnames(tmp)[3] <- "現預金"  # 途中で列名が変わっているので
+  return(tmp)
+}
+
+data <- sapply(files, myreadcsv) %>%
+  bind_rows() %>%
+  replace_na(list(FX = 0)) %>%
+  mutate(日付 = as.Date(日付)) %>%
+  mutate(現預金 = 現預金 + FX + ポイント) %>%
+  select(-c(FX, ポイント)) %>%
+  pivot_longer(-c(日付, 合計), names_to="type", values_to="value") %>%
+  mutate(type = factor(type, levels=c("株式", "投資信託", "現預金")))  # 描画の順序制御用
+
+g <- ggplot(data=data) +
+  geom_area(mapping = aes(x = 日付, y = value, group = type, colour = type, fill = type)) +
+  scale_y_continuous(label = scales::comma) +  # y軸をカンマ区切りに
+  scale_x_date(date_breaks = "30 day", date_labels = "%Y/%m/%d") +  # x軸の目盛を減らす
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # 目盛を斜めに
+plot(g)
